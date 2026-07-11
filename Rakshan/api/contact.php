e9e8c872a8f7e87f0dbc143ef473c8f38a015fd6
sasi2207 <sasikumarp2207@gmail.com>
2026-07-11 10:41:52 +0530

    Update About Page

--- Rakshan/api/contact.php
+++ Rakshan/api/contact.php
@@ -30,6 +30,11 @@ if (!file_exists($dbPath)) {
 }
 require $dbPath;
 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Read JSON Input payload
 $data = json_decode(file_get_contents("php://input"), true);
 
